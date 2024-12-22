d16541ddd3f38bba3da929b26ccd6f3870173cfe
Pavel <pavel5140@yandex.ru>
2024-12-22 18:46:02 +0300

    public

--- app/Console/Commands/UpdateSite.php
+++ app/Console/Commands/UpdateSite.php
@@ -2,36 +2,146 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Http;
 use Symfony\Component\Process\Process;
-use Illuminate\Support\Facades\Config;
 
 class UpdateSite extends Command
 {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
     protected $signature = 'git:pull';
-    protected $description = 'Выполняет git pull origin master';
 
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Pull files from GIT';
+
+    /**
+     * Is the code already updated or not
+     * 
+     * @var boolean
+     */
+    private $alreadyUpToDate;
+
+    /**
+     * Log from git pull
+     * 
+     * @var array
+     */
+    private $pullLog = [];
+
+    /**
+     * Log from composer install
+     * 
+     * @var boolean
+     */
+    private $composerLog = [];
+
+
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
     public function handle()
     {
-        $this->info('Начинаю git pull...');
 
-        $process = new Process(['git', 'pull', 'origin', 'master']);
-        $process->setWorkingDirectory(base_path()); //  очень важно
-        
-        try {
-            $process->run();
-            dd($process);
-            if (!$process->isSuccessful()) {
-                
-              $this->error("Ошибка при git pull: ". $process->getErrorOutput());
-               return false;
+        if(!$this->runPull()) {
+
+            $this->error("An error occurred while executing 'git pull'. \nLogs:");
+            
+            foreach($this->pullLog as $logLine) {
+                $this->info($logLine);
             }
-            dd($process);
-            $this->info('Git pull успешно выполнен!');
-             return true;
-        } catch (ProcessFailedException $exception) {
-            $this->error('Git pull не удался: ' . $exception->getMessage());
-             return false;
+
+            return;
         }
+
+
+        if($this->alreadyUpToDate) {
+            $this->info("The application is already up-to-date");
+            return;
+        }
+
+
+        if(!$this->runComposer()) {
+
+            $this->error("Error while updating composer files. \nLogs:");
+
+            foreach($this->composerLog as $logLine) {
+                $this->info($logLine);
+            }
+
+            return;
+        }
+
+        $this->info("Succesfully updated the application.");
+
+
     }
+
+
+
+
+    /**
+     * Run git pull process
+     * 
+     * @return boolean
+     */
+
+    private function runPull()
+    {
+
+        $process = new Process(['git', 'pull','origin','master']);
+        $this->info("Running 'git pull'");
+        
+        $process->run();
+        dd($process->getErrorOutput());
+        return $process->isSuccessful();
+
+    }
+
+
+
+    /**
+     * Run composer install process
+     * 
+     * @return boolean
+     */
+
+    private function runComposer()
+    {
+
+        $process = new Process('composer install');
+        $this->info("Running 'composer install'");
+
+        $process->run(function($type, $buffer) {
+            $this->composerLog[] = $buffer;
+        });
+
+
+        return $process->isSuccessful();
+
+
+
+    }
+
+
 }
